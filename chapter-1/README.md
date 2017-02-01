b43ca51e6d7f96a45c25ce32a5a72a2bbb8847b0
abdullah <writer-edi@hotmail.com>
2017-02-01 20:13:40 +0300

    Functions as first class objects

---
+++ chapter-1/README.md
@@ -0,0 +1,8 @@
+#Notlar
+Double negation cast yapar -- bunun anlamı string veya number olan bir tipi boolean'a çevirir
+
+```javascript
+    var num = 0 
+    var boll = !!num
+    //boll false
+```
---
+++ chapter-1/functions.js
@@ -0,0 +1,40 @@
+
+/** 
+ * Executed Immediately
+ * Anında çalışan anonymous fonsksiyon -- Magic method olarakta geçiyor
+ */
+(function(){
+    var foo = 10;
+    var bar = 2;
+    console.log(foo * bar)
+})()
+
+
+/**
+ * Parametre alabilir
+ * Bir değişkene atanabilir
+ */
+var baz =  (function(foo,bar){
+    return foo * bar
+})(20,2)
+// Baz 40 yapar
+console.log(baz)
+
+
+//Closuere protected variable space
+/** 
+ * JavaScript function level scope'a sahiptir
+ * baz2 çalıştırıldığında 20 sonucunu verecek fakat foo ve bar'a ulaşamayacaktır 
+ * çünkü foo ve var anonymous fonsksiyon'un içinde tanımlandı
+ */
+var baz2;
+
+(function(){
+    var foo = 10;
+    var bar = 2;
+    baz2 = function(){
+        return foo * bar
+    }
+})();
+
+console.log(baz2())
